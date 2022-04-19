9cc497c36850e4577fe09d128410202a10472b3d
ply2782 <ply2782@daum.net>
2022-04-19 23:10:37 +0900

    Initial Commit

---
+++ JunTalk2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JunTalk2
+//
+//  Created by 바틀 on 2022/04/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
